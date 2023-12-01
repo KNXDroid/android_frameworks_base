7d2176c1b3db21d5d432d3bfd3c590da0a9cc47f
Zhongcui Peng <pengzhongcui@xiaomi.corp-partner.google.com>
2023-12-01 07:25:10 +0000

    Fix process memory data during dump
    Assign hasSwapPss to true as long as there is a process with swap pss
    
    Google: 2855566
    Change-Id: Ia72167177c8721941df13dbd46cace3d92406fe6
    BUG:314245234

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -12352,7 +12352,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                         continue;
                     }
                     endTime = SystemClock.currentThreadTimeMillis();
-                    hasSwapPss = mi.hasSwappedOutPss;
+                    hasSwapPss = hasSwapPss || mi.hasSwappedOutPss;
                     memtrackGraphics = mi.getOtherPrivate(Debug.MemoryInfo.OTHER_GRAPHICS);
                     memtrackGl = mi.getOtherPrivate(Debug.MemoryInfo.OTHER_GL);
                 } else {
@@ -13028,7 +13028,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                     continue;
                 }
                 endTime = SystemClock.currentThreadTimeMillis();
-                hasSwapPss = mi.hasSwappedOutPss;
+                hasSwapPss = hasSwapPss || mi.hasSwappedOutPss;
             } else {
                 reportType = ProcessStats.ADD_PSS_EXTERNAL;
                 startTime = SystemClock.currentThreadTimeMillis();
