03dc7beb92f7487763eadcc2dc028df194b9a4e4
danya2271 <dany2271@yandex.ru>
2024-12-18 21:58:52 +0500

    fixup! EventLog: Disable event writing in production builds

--- core/java/android/util/EventLog.java
+++ core/java/android/util/EventLog.java
@@ -340,7 +340,6 @@ public class EventLog {
      * @param value A value to log
      * @return The number of bytes written
      */
-    @RavenwoodRedirect
     public static int writeEvent(int tag, int value) {
         if (!Build.IS_DEBUGGABLE) {
             return 0;
