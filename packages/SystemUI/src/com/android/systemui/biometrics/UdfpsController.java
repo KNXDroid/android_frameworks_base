433268dba23b97d334aeeb18f566d8a9a37c746a
danya2271 <dany2271@yandex.ru>
2025-02-22 22:27:49 +0500

    Revert "SystemUI: Send load-up hint on finger down"
    This reverts commit e434c83745c818fe571a5394d6f810ef78463998.

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -1098,13 +1098,6 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                     + " current: " + mOverlay.getRequestId());
             return;
         }
-
-        final View view = mOverlay.getTouchOverlay();
-
-        if (view != null && view.getViewRootImpl() != null) {
-            view.getViewRootImpl().notifyRendererOfExpensiveFrame();
-        }
-
         if (isOptical()) {
             mLatencyTracker.onActionStart(ACTION_UDFPS_ILLUMINATE);
         }
@@ -1122,6 +1115,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 minor, major, orientation, time, gestureStart, isAod);
         Trace.endAsyncSection("UdfpsController.e2e.onPointerDown", 0);
 
+        final View view = mOverlay.getTouchOverlay();
         if (view != null && isOptical()) {
             if (mIgnoreRefreshRate) {
                 dispatchOnUiReady(requestId);
@@ -1138,10 +1132,6 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             for (Callback cb : mCallbacks) {
                 cb.onFingerDown();
             }
-        }
-
-        if (view != null && view.getViewRootImpl() != null) {
-            view.getViewRootImpl().notifyRendererOfExpensiveFrame();
         }
     }
 
