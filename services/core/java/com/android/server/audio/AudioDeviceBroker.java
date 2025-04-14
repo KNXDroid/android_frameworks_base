6dc3c47afac468c99036386b039aba57e872cc0f
aosong.kuang <aosong.kuang@unisoc.com>
2025-04-14 10:23:15 +0800

    getCommunicationDevice() will wait after device update end
    Google: 3584311
    Change-Id: I228a546f75127ae9420ed0425eb53fb767589d36

--- services/core/java/com/android/server/audio/AudioDeviceBroker.java
+++ services/core/java/com/android/server/audio/AudioDeviceBroker.java
@@ -2052,7 +2052,7 @@ public class AudioDeviceBroker {
                             Log.e(TAG, "mCommunicationDeviceUpdateCount already 0 in"
                                     + " MSG_L_SET_COMMUNICATION_DEVICE_FOR_CLIENT");
                         }
-                        mCommunicationDeviceLock.notify();
+                        mCommunicationDeviceLock.notifyAll();
                     }
                     break;
 
