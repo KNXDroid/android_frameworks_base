6f45716f0b16589e4d8f9f52423e0aac13b41200
danya2271 <dany2271@yandex.ru>
2025-06-20 18:41:17 +0500

    Revert "SystemUI: force disable scrim corner radius"
    This reverts commit 9189df62e43e9e96a1a75e5cf00857c07679b0eb.

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
@@ -438,6 +438,8 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
         mTouchSlop = configuration.getScaledTouchSlop();
         mSlopMultiplier = configuration.getScaledAmbiguousGestureMultiplier();
         mStatusBarMinHeight = SystemBarUtils.getStatusBarHeight(mPanelView.getContext());
+        mScrimCornerRadius = mResources.getDimensionPixelSize(
+                R.dimen.notification_scrim_corner_radius);
         mScreenCornerRadius = (int) ScreenDecorationsUtils.getWindowCornerRadius(
                 mPanelView.getContext());
         mFalsingThreshold = mResources.getDimensionPixelSize(R.dimen.qs_falsing_threshold);
@@ -1303,7 +1305,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
 
     private void applyClippingImmediately(int left, int top, int right, int bottom,
             boolean qsVisible) {
-        int radius = 0;
+        int radius = mScrimCornerRadius;
         boolean clipStatusView = false;
         mLastClipBounds.set(left, top, right, bottom);
         if (mIsFullWidth) {
@@ -1311,8 +1313,8 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
             float screenCornerRadius =
                     mRecordingController.isRecording() || mCastController.hasConnectedCastDevice()
                             ? 0 : mScreenCornerRadius;
-            radius = (int) MathUtils.lerp(screenCornerRadius, radius,
-                    Math.min(top / (float) radius, 1f));
+            radius = (int) MathUtils.lerp(screenCornerRadius, mScrimCornerRadius,
+                    Math.min(top / (float) mScrimCornerRadius, 1f));
 
             float bottomRadius = mSplitShadeEnabled ? screenCornerRadius : 0;
             if (!getExpanded()) {
@@ -1391,7 +1393,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
      */
     @VisibleForTesting
     int calculateBottomCornerRadius(float screenCornerRadius) {
-        return (int) MathUtils.lerp(screenCornerRadius, 0,
+        return (int) MathUtils.lerp(screenCornerRadius, mScrimCornerRadius,
                 Math.min(calculateBottomRadiusProgress(), 1f));
     }
 
@@ -1402,7 +1404,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
 
     @VisibleForTesting
     int getScrimCornerRadius() {
-        return 0;
+        return mScrimCornerRadius;
     }
 
     void setDisplayInsets(int leftInset, int rightInset) {
--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/ui/viewbinder/NotificationScrollViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/ui/viewbinder/NotificationScrollViewBinder.kt
@@ -35,7 +35,6 @@ import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.DisposableHandle
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
@@ -123,5 +122,5 @@ constructor(
 
     /** flow of the scrim clipping radius */
     private val scrimRadius: Flow<Int>
-        get() = flowOf(0)
+        get() = configuration.getDimensionPixelOffset(R.dimen.notification_scrim_corner_radius)
 }
